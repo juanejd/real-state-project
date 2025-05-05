2cb3dad698c944415ed52b042e628873ac4a09cd
Juan Esteban Jiménez Daza <47701692+juanejd@users.noreply.github.com>
2025-05-05 03:18:15 -0500

    Add files via upload

---
+++ includes/config/database.php
@@ -0,0 +1,13 @@
+<?php 
+
+function conectarDB() : mysqli {
+    $db = mysqli_connect('localhost', 'root', 'root', 'bienesraices_crud');
+
+    if(!$db) {
+        echo "Error no se pudo conectar";
+        exit;
+    } 
+
+    return $db;
+    
+}
